1879925769d2864936c95a4662234a9c43822087
m0ph <morris.peichl@gmx.de>
2024-02-15 11:24:15 +0100

    removed oidc endpoint from aws lb tf

--- aws-lb-ctl.tf
+++ aws-lb-ctl.tf
@@ -8,7 +8,7 @@ resource "kubernetes_service_account" "service-account" {
     }
     annotations = {
       "eks.amazonaws.com/role-arn"               = aws_iam_role.lb_role_trust.arn
-      "eks.amazonaws.com/sts-regional-endpoints" = "true"
+      //"eks.amazonaws.com/sts-regional-endpoints" = "true"
     }
   }
 }
